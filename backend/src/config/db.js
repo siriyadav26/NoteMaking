2d2e8421c8c45b9a3d70d0c904ed349eb08feb7f
Siri V <your-github-yadavsiri74@gmail.com>
2026-03-09 20:29:32 +0530

    Removed node_modules and added gitignore

---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+
+export const connectdB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("MONGODB CONNECTED SUCCESSFULLY!");
+    }catch(error){
+        console.log("Error connecting to MONGODB",error);
+        process.exit(1);//exit with failure
+    }
+};
