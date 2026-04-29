899cae217fd0dd34316b6d0a845572ea1d1d097d
Siri V <your-github-yadavsiri74@gmail.com>
2026-04-29 15:07:23 +0530

    server.js changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectdB } from "./config/db.js";
@@ -10,7 +11,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(express.json());
